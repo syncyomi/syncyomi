09af815be4846e46d7cb11a6a5c136f09679f5ca
KaiserBh <kaiserbh@proton.me>
2023-04-07 02:06:47 +1000

    feat: added database and apikey/authkey

---
+++ internal/database/database.go
@@ -0,0 +1,124 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	sq "github.com/Masterminds/squirrel"
+	"github.com/kaiserbh/tachiyomi-sync-server/internal/domain"
+	"github.com/kaiserbh/tachiyomi-sync-server/internal/logger"
+	"github.com/kaiserbh/tachiyomi-sync-server/pkg/errors"
+	"github.com/rs/zerolog"
+	"sync"
+)
+
+var databaseDriver = "postgres"
+
+type DB struct {
+	log     zerolog.Logger
+	handler *sql.DB
+	lock    sync.RWMutex
+	ctx     context.Context
+	cancel  func()
+
+	Driver string
+	DSN    string
+
+	squirrel sq.StatementBuilderType
+}
+
+func NewDB(cfg *domain.Config, log logger.Logger) (*DB, error) {
+	db := &DB{
+		// set default placeholder for squirrel to support both sqlite and postgres
+		squirrel: sq.StatementBuilder.PlaceholderFormat(sq.Dollar),
+		log:      log.With().Str("module", "database").Logger(),
+	}
+	db.ctx, db.cancel = context.WithCancel(context.Background())
+
+	switch cfg.DatabaseType {
+	case "sqlite":
+		databaseDriver = "sqlite"
+		db.Driver = "sqlite"
+		db.DSN = dataSourceName(cfg.ConfigPath, "tachi-sync-server.db")
+	case "postgres":
+		if cfg.PostgresHost == "" || cfg.PostgresPort == 0 || cfg.PostgresDatabase == "" {
+			return nil, errors.New("postgres: bad variables")
+		}
+		db.DSN = fmt.Sprintf("postgres://%v:%v@%v:%d/%v?sslmode=disable", cfg.PostgresUser, cfg.PostgresPass, cfg.PostgresHost, cfg.PostgresPort, cfg.PostgresDatabase)
+		db.Driver = "postgres"
+		databaseDriver = "postgres"
+	default:
+		return nil, errors.New("unsupported database: %v", cfg.DatabaseType)
+	}
+
+	return db, nil
+}
+
+func (db *DB) Open() error {
+	if db.DSN == "" {
+		return errors.New("DSN required")
+	}
+
+	var err error
+
+	switch db.Driver {
+	case "sqlite":
+		if err = db.openSQLite(); err != nil {
+			db.log.Fatal().Err(err).Msg("could not open sqlite db connection")
+			return err
+		}
+	case "postgres":
+		if err = db.openPostgres(); err != nil {
+			db.log.Fatal().Err(err).Msg("could not open postgres db connection")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (db *DB) Close() error {
+	// cancel background context
+	db.cancel()
+
+	// close database
+	if db.handler != nil {
+		return db.handler.Close()
+	}
+	return nil
+}
+
+func (db *DB) Ping() error {
+	return db.handler.Ping()
+}
+
+func (db *DB) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
+	tx, err := db.handler.BeginTx(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Tx{
+		Tx:      tx,
+		handler: db,
+	}, nil
+}
+
+type Tx struct {
+	*sql.Tx
+	handler *DB
+}
+
+type ILikeDynamic interface {
+	ToSql() (sql string, args []interface{}, err error)
+}
+
+// ILike is a wrapper for sq.Like and sq.ILike
+// SQLite does not support ILike but postgres does so this checks what database is being used
+func ILike(col string, val string) ILikeDynamic {
+	if databaseDriver == "sqlite" {
+		return sq.Like{col: val}
+	}
+
+	return sq.ILike{col: val}
+}
---
+++ internal/http/auth.go
@@ -0,0 +1,170 @@
+package http
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/sessions"
+	"github.com/kaiserbh/tachiyomi-sync-server/internal/domain"
+	"github.com/rs/zerolog"
+	"net/http"
+)
+
+type authService interface {
+	GetUserCount(ctx context.Context) (int, error)
+	Login(ctx context.Context, username, password string) (*domain.User, error)
+	CreateUser(ctx context.Context, username, password string) error
+}
+
+type authHandler struct {
+	log     zerolog.Logger
+	encoder encoder
+	config  *domain.Config
+	service authService
+
+	cookieStore *sessions.CookieStore
+}
+
+func newAuthHandler(encoder encoder, log zerolog.Logger, config *domain.Config, cookieStore *sessions.CookieStore, service authService) *authHandler {
+	return &authHandler{
+		log:         log,
+		encoder:     encoder,
+		config:      config,
+		service:     service,
+		cookieStore: cookieStore,
+	}
+}
+
+func (h authHandler) Routes(r chi.Router) {
+	r.Post("/login", h.login)
+	r.Post("/logout", h.logout)
+	r.Post("/onboard", h.onboard)
+	r.Get("/onboard", h.canOnboard)
+	r.Get("/validate", h.validate)
+}
+
+func (h authHandler) login(w http.ResponseWriter, r *http.Request) {
+	var (
+		ctx  = r.Context()
+		data domain.User
+	)
+
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		// encode error
+		h.encoder.StatusResponse(ctx, w, nil, http.StatusBadRequest)
+		return
+	}
+
+	h.cookieStore.Options.HttpOnly = true
+	h.cookieStore.Options.SameSite = http.SameSiteLaxMode
+	h.cookieStore.Options.Path = h.config.BaseURL
+
+	// tachi-sync-server does not support serving on TLS / https, so this is only available behind reverse proxy
+	// if forwarded protocol is https then set cookie secure
+	// SameSite Strict can only be set with a secure cookie. So we overwrite it here if possible.
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+	fwdProto := r.Header.Get("X-Forwarded-Proto")
+	if fwdProto == "https" {
+		h.cookieStore.Options.Secure = true
+		h.cookieStore.Options.SameSite = http.SameSiteStrictMode
+	}
+
+	session, _ := h.cookieStore.Get(r, "user_session")
+
+	_, err := h.service.Login(ctx, data.Username, data.Password)
+	if err != nil {
+		h.log.Error().Err(err).Msgf("Auth: Failed login attempt username: [%s] ip: %s", data.Username, ReadUserIP(r))
+		h.encoder.StatusResponse(ctx, w, nil, http.StatusUnauthorized)
+		return
+	}
+
+	// Set user as authenticated
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+
+	h.encoder.StatusResponse(ctx, w, nil, http.StatusNoContent)
+}
+
+func (h authHandler) logout(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	session, _ := h.cookieStore.Get(r, "user_session")
+
+	// Revoke users authentication
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+
+	h.encoder.StatusResponse(ctx, w, nil, http.StatusNoContent)
+}
+
+func (h authHandler) onboard(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	session, _ := h.cookieStore.Get(r, "user_session")
+
+	// Don't proceed if user is authenticated
+	if _, ok := session.Values["authenticated"].(bool); ok {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	var data domain.User
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		// encode error
+		h.encoder.StatusResponse(ctx, w, nil, http.StatusBadRequest)
+		return
+	}
+
+	err := h.service.CreateUser(ctx, data.Username, data.Password)
+	if err != nil {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	// send empty response as ok
+	h.encoder.StatusResponse(ctx, w, nil, http.StatusNoContent)
+}
+
+func (h authHandler) canOnboard(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	userCount, err := h.service.GetUserCount(ctx)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if userCount > 0 {
+		// send 503 service onboarding unavailable
+		http.Error(w, "Onboarding unavailable", http.StatusForbidden)
+		return
+	}
+
+	// send empty response as ok
+	// (client can proceed with redirection to onboarding page)
+	h.encoder.StatusResponse(ctx, w, nil, http.StatusNoContent)
+}
+
+func (h authHandler) validate(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	session, _ := h.cookieStore.Get(r, "user_session")
+
+	// Check if user is authenticated
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusUnauthorized)
+		return
+	}
+
+	// send empty response as ok
+	h.encoder.StatusResponse(ctx, w, nil, http.StatusNoContent)
+}
+
+func ReadUserIP(r *http.Request) string {
+	IPAddress := r.Header.Get("X-Real-Ip")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IPAddress == "" {
+		IPAddress = r.RemoteAddr
+	}
+	return IPAddress
+}
