44b7e7be3fa86e9712293e963c310e8a3b4b067f
KaiserBh <kaiserbh@proton.me>
2023-04-06 21:36:07 +1000

    feat:added logger and config

---
+++ internal/domain/config.go
@@ -0,0 +1,30 @@
+package domain
+
+type Config struct {
+	Version          string
+	ConfigPath       string
+	Host             string `toml:"host"`
+	Port             int    `toml:"port"`
+	LogLevel         string `toml:"logLevel"`
+	LogPath          string `toml:"logPath"`
+	LogMaxSize       int    `toml:"logMaxSize"`
+	LogMaxBackups    int    `toml:"logMaxBackups"`
+	BaseURL          string `toml:"baseUrl"`
+	SessionSecret    string `toml:"sessionSecret"`
+	CheckForUpdates  bool   `toml:"checkForUpdates"`
+	DatabaseType     string `toml:"databaseType"`
+	PostgresHost     string `toml:"postgresHost"`
+	PostgresPort     int    `toml:"postgresPort"`
+	PostgresDatabase string `toml:"postgresDatabase"`
+	PostgresUser     string `toml:"postgresUser"`
+	PostgresPass     string `toml:"postgresPass"`
+}
+
+type ConfigUpdate struct {
+	Host            *string `json:"host,omitempty"`
+	Port            *int    `json:"port,omitempty"`
+	LogLevel        *string `json:"log_level,omitempty"`
+	LogPath         *string `json:"log_path,omitempty"`
+	BaseURL         *string `json:"base_url,omitempty"`
+	CheckForUpdates *bool   `json:"check_for_updates,omitempty"`
+}
