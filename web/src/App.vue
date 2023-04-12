d4af03b9b70b33dfcf6026e04fea4880db401f4f
KaiserBh <kaiserbh@proton.me>
2023-04-13 00:06:29 +1000

    refactor: we don't need those hacky way with vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,17 +7,4 @@
   </v-app>
 </template>
 
-<script lang="ts" setup>
-import { computed, ref, watchEffect } from "vue";
-import { useRoute } from "vue-router";
-import NavBar from "@/components/NavBar.vue";
-
-const route = useRoute();
-const showAppBar = ref(false);
-
-const disableAppBarRoutes: string[] = ["Login", "Onboard"];
-
-watchEffect(() => {
-  showAppBar.value = !disableAppBarRoutes.includes(route.name as string);
-});
-</script>
+<script lang="ts" setup></script>
