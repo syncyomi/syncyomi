f41f4bf99f761ddeb5b52a19e46c2307e1f5c763
KaiserBh <kaiserbh@proton.me>
2023-04-13 00:10:30 +1000

    chore: added portal plugin

--- web/src/plugins/index.ts
+++ web/src/plugins/index.ts
@@ -5,20 +5,17 @@
  */
 
 // Plugins
-import { loadFonts } from './webfontloader'
-import vuetify from './vuetify'
-import pinia from '../store'
-import router from '../router'
+import { loadFonts } from "./webfontloader";
+import vuetify from "./vuetify";
+import pinia from "../store";
+import router from "../router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
+import PortalVue from "portal-vue";
 
 // Types
-import type { App } from 'vue'
+import type { App } from "vue";
 
-export function registerPlugins (app: App) {
-  loadFonts()
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
-    .use(VueQueryPlugin)
+export function registerPlugins(app: App) {
+  loadFonts();
+  app.use(vuetify).use(router).use(pinia).use(VueQueryPlugin).use(PortalVue);
 }
