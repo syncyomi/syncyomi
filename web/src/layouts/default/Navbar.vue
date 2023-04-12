783e6f700d2e7eec3fae2b85bf7393efddf95736
KaiserBh <kaiserbh@proton.me>
2023-04-12 21:06:46 +1000

    refactor: add support for mobile.

--- web/src/layouts/default/Navbar.vue
+++ web/src/layouts/default/Navbar.vue
@@ -4,52 +4,79 @@
       TachiyomiSync
     </v-toolbar-title>
 
-    <v-toolbar-items>
-      <v-col>
-        <v-btn
-          height="100%"
-          rounded
-          size="large"
-          to="/"
-          variant="flat"
-          width="100%"
-        >
-          Dashboard
-        </v-btn>
-      </v-col>
+    <template v-if="showToolbarItems">
+      <v-toolbar-items>
+        <v-col>
+          <v-btn
+            height="100%"
+            rounded
+            size="large"
+            to="/"
+            variant="flat"
+            width="100%"
+          >
+            Dashboard
+          </v-btn>
+        </v-col>
 
-      <v-col>
-        <v-btn
-          height="100%"
-          rounded
-          size="large"
-          to="logs"
-          variant="flat"
-          width="100%"
-        >
-          Logs
-        </v-btn>
-      </v-col>
+        <v-col>
+          <v-btn
+            height="100%"
+            rounded
+            size="large"
+            to="logs"
+            variant="flat"
+            width="100%"
+          >
+            Logs
+          </v-btn>
+        </v-col>
 
-      <v-col>
-        <v-btn
-          height="100%"
-          rounded
-          size="large"
-          to="settings"
-          variant="flat"
-          width="100%"
-        >
-          Settings
-        </v-btn>
-      </v-col>
-    </v-toolbar-items>
+        <v-col>
+          <v-btn
+            height="100%"
+            rounded
+            size="large"
+            to="settings"
+            variant="flat"
+            width="100%"
+          >
+            Settings
+          </v-btn>
+        </v-col>
+      </v-toolbar-items>
+    </template>
+
     <v-spacer></v-spacer>
   </v-app-bar>
+
+  <template v-if="!showToolbarItems">
+    <v-bottom-navigation grow>
+      <v-btn to="/" value="dashboard">
+        <v-icon>mdi-view-dashboard</v-icon>
+        Dashboard
+      </v-btn>
+
+      <v-btn to="logs" value="logs">
+        <v-icon>mdi-text-box-multiple-outline</v-icon>
+        Logs
+      </v-btn>
+
+      <v-btn to="settings" value="settings">
+        <v-icon>mdi-cog-outline</v-icon>
+        Settings
+      </v-btn>
+    </v-bottom-navigation>
+  </template>
 </template>
 
 <script lang="ts" setup>
-//
-</script>
+import { useDisplay } from "vuetify";
+import { computed } from "vue";
 
-<style scoped></style>
+const { width } = useDisplay();
+
+const showToolbarItems = computed(() => {
+  return width.value > 700;
+});
+</script>
