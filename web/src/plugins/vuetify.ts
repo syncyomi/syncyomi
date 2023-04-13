d153cef89b07859017f05b7ea623a453254b1afb
KaiserBh <kaiserbh@proton.me>
2023-04-14 02:38:59 +1000

    refactor: vuetify,ts
    Use double quotes.
    
    Signed-off-by: KaiserBh <kaiserbh@proton.me>

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -5,23 +5,23 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: "dark",
     themes: {
       light: {
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+          primary: "#1867C0",
+          secondary: "#5CBBF6",
         },
       },
     },
   },
-})
+});
