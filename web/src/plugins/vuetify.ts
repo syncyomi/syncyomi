7c0b02f9fb5ff50b107ee8f53e7a546d3d2c6a31
KaiserBh <kaiserbh@proton.me>
2023-04-17 01:57:59 +1000

    feat: added fa icons
    Signed-off-by: KaiserBh <kaiserbh@proton.me>

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -6,13 +6,23 @@
 
 // Styles
 import "@mdi/font/css/materialdesignicons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import "vuetify/styles";
 
 // Composables
 import { createVuetify } from "vuetify";
+import { fa } from "vuetify/iconsets/fa";
+import { mdi } from "vuetify/iconsets/mdi";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    sets: {
+      mdi,
+      fa,
+    },
+  },
   theme: {
     defaultTheme: "dark",
     themes: {
