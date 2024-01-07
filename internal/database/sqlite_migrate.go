eb843b6798b2cd8a1eac7f2841b948b16a8e5f4a
KaiserBh <41852205+KaiserBh@users.noreply.github.com>
2024-01-07 19:05:11 +1100

    fix: sync lock loop (#26)
    * fix: sync-lock loop.
    
    We could have fixed this in the client side, but fixing in the server side is easier.
    
    * fix: migrate sqlite version

--- internal/database/sqlite_migrate.go
+++ internal/database/sqlite_migrate.go
@@ -121,4 +121,24 @@ var sqliteMigrations = []string{
 		FOREIGN KEY (user_api_key) REFERENCES api_key (key) ON DELETE CASCADE
 	);
 `,
+	`
+    ALTER TABLE sync_lock RENAME TO _sync_lock_old;
+    CREATE TABLE sync_lock
+    (
+        id INTEGER PRIMARY KEY,
+        user_api_key TEXT UNIQUE,
+        acquired_by TEXT UNIQUE,
+        last_sync TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        status TEXT NOT NULL DEFAULT 'success',  -- Changed to 'success'
+        retry_count INT NOT NULL DEFAULT 0,
+        acquired_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_api_key) REFERENCES api_key (key) ON DELETE CASCADE
+    );
+    INSERT INTO sync_lock (id, user_api_key, acquired_by, last_sync, status, retry_count, acquired_at, expires_at, created_at, updated_at)
+        SELECT id, user_api_key, acquired_by, last_sync, 'success', retry_count, acquired_at, expires_at, created_at, updated_at FROM _sync_lock_old;
+    DROP TABLE _sync_lock_old;
+    `,
 }
